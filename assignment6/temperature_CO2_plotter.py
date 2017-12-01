d65750ac367e83f04f252c1b616a09b16b2d0572
Tommy Myrvik <tommy_myrvik@hotmail.com>
2017-12-01 23:55:07 +0100

    Added full (almost completed) project

--- assignment6/temperature_CO2_plotter.py
+++ assignment6/temperature_CO2_plotter.py
@@ -2,9 +2,21 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 from scipy import stats
+from io import BytesIO
 
 def plot_temperature(month, start_year, end_year, y_axis=None, show_plot=False):
+	"""This function takes a month and a start year and end year as required
+	arguments. It generates a Pandas dataframe of all data found in the file
+	temperature.csv, and plots the data corresponding to the month and the range
+	of the years given as arguments. 
+	One also has the possibility to change the y-axis, but if no argument is given
+	it's just set to default. 
+	There is also an argument show_plot that's set to False as default to avoid 
+	the plot to pop up from the terminal when the program is run on the website.
+	A bytestring cointaing the bytes of the plot will be generated in this case
+	instead of saving the image as a png/jpg-file.
 
+	"""
 	temp_stats = pd.read_csv('temperature.csv', sep=',')
 
 	first_year = temp_stats['Year'][0]
@@ -14,9 +26,15 @@ def plot_temperature(month, start_year, end_year, y_axis=None, show_plot=False):
 	temp_stats[start_idx:end_idx].plot(x='Year', y=month, ylim=y_axis)
 	plt.ylabel('Temperature (C)')
 	plt.title('Average temperature in US %g-%g' %(start_year, end_year))
-	plt.savefig("static/Temp.png")
 	if show_plot:
 		plt.show()
+	else:
+		Tempfile = BytesIO()
+		plt.savefig(Tempfile, format='png')
+		Tempfile.seek(0)  # rewind to beginning of file
+		import base64
+		Tempdata_png = base64.b64encode(Tempfile.getvalue()).decode()
+		return Tempdata_png
 
 
 
@@ -31,31 +49,40 @@ def plot_CO2(start_year, end_year, y_axis=None, show_plot=False):
 	CO2_stats[start_idx: end_idx].plot(x='Year', y='Carbon')
 	plt.ylabel('CO2 emission (MMT)')
 	plt.title('Global CO2 emmisions %g-%g' % (start_year, end_year))
-	plt.savefig('static/CO2.png')
 	if show_plot:
 		plt.show()
+	else:
+		CO2file = BytesIO()
+		plt.savefig(CO2file, format='png')
+		CO2file.seek(0)  # rewind to beginning of file
+		import base64
+		CO2data_png = base64.b64encode(CO2file.getvalue()).decode()
+		return CO2data_png
 
 
-def plot_CO2_country(years, low_thresh=0, upp_thresh=10000, show_plot=False):
+def plot_CO2_country(year, low_thresh=0, upp_thresh=10000, show_plot=False):
 
-	if type(years) in [float, int]:
-		years = str(years)
-	elif type(years) in [list, tuple]:
-		years = [str(year) for year in years]
+	year=str(year)
 
 	CO2_cntr_stats = pd.read_csv('CO2_by_country.csv', sep=',')
 
+	thresholds = (upp_thresh > CO2_cntr_stats[year]) & (CO2_cntr_stats[year] > low_thresh)
 
-	thresholds = (upp_thresh > CO2_cntr_stats[years]) & (CO2_cntr_stats[years] > low_thresh)
-
-	#thresholds = upp_thresh > CO2_cntr_stats['2012'] > low_thresh
-
-	CO2_cntr_stats[thresholds].plot(x='Country Code', y=years, kind='bar')
+	CO2_cntr_stats[thresholds].plot(x='Country Code', y=year, kind='bar')
+	plt.ylabel('CO2 emission (MMT)')
+	plt.title('Countries with CO2 emmisions in range %g-%g MMT (%s)' % (low_thresh, upp_thresh, year))
 
 	if show_plot:
 		plt.show()
+	else:
+		Countryfile = BytesIO()
+		plt.savefig(Countryfile, format='png')
+		Countryfile.seek(0)  # rewind to beginning of file
+		import base64
+		Countrydata_png = base64.b64encode(Countryfile.getvalue()).decode()
+		return Countrydata_png
 
-def plot_future(year, month):
+def plot_future(year, month, show_plot=False):
 
 	CO2_stats = pd.read_csv('co2.csv', sep=',')
 
@@ -63,52 +90,54 @@ def plot_future(year, month):
 	sec_last_CO2 = CO2_stats['Carbon'].values[-2]
 	diff_CO2 = last_CO2 - sec_last_CO2
 
-	first_year = CO2_stats['Year'].values[0]
 	last_year = CO2_stats['Year'].values[-1]
-
 	no_years = year-last_year
 
 
 	CO2_fut = np.array([last_CO2 + i*diff_CO2 for i in range(no_years + 1)])
 	years_fut = np.array([last_year + i for i in range(no_years + 1)])
 
-	CO2_future = pd.DataFrame({'Year': years_fut, 'Carbon': CO2_fut})
-	print(CO2_future)
-	'''
-	ax = CO2_stats[-10:].plot(x='Year', y='Carbon', label='Data')
-	plt.ylabel('CO2 emission (MMT)')
-	CO2_future.plot(x='Year', y='Carbon', ax=ax, label='Predicted future')
-	plt.show()
-	'''
+
 	temp_stats = pd.read_csv('temperature.csv', sep=',')
 
 
 	#--------------------------------------
 	# Temp is linear of CO2, T(x) = ax + b
-	# Finding 'a' by interpolating 10 steps
+	# Finding 'a' and 'b by interpolating 30 steps using linear regression
 	#--------------------------------------------
 
-	T = np.array(temp_stats[month].tail(30))
+	temps_past = np.array(temp_stats[month].tail(30))
 	CO2 = np.array(CO2_stats['Carbon'].tail(30))
 	years = np.array(CO2_stats['Year'].tail(30))
 
-	#---------------
-	# Simple method
-	#a = (T[-1] - T[0])/(CO2[-1] - CO2[0])
-	#---------------
 
 	#Linear regression (The last three values returned from the function
 	# will not be used.)
 	#--------------------
-	a, b, r_value, p_value, std_err = stats.linregress(CO2, T)
+	a, b, r_value, p_value, std_err = stats.linregress(CO2, temps_past)
 
 
 	temps = a*CO2 + b
-	temps_fut = a*(CO2_fut-CO2_fut[0]) + T[-1]
+	temps_fut = a*(CO2_fut-CO2_fut[0]) + temps_past[-1]
 
-	plt.plot(years, T)
-	plt.plot(years_fut, temps_fut)
-	plt.show()
+	Temp = pd.DataFrame({'Year': years, 'Temp': temps_past})
+	Temp_future = pd.DataFrame({'Year': years_fut, 'Temp': temps_fut})
+
+
+	ax = Temp.plot(x='Year', y='Temp', label="Data")
+	Temp_future.plot(x='Year', y='Temp', ax=ax, label="Future")
+	plt.ylabel("Temperature(C)")
+	plt.title("Predicted temperature of %s up to year %g" % (month, year))
+
+	if show_plot:
+		plt.show()
+	else:
+		Futurefile = BytesIO()
+		plt.savefig(Futurefile, format='png')
+		Futurefile.seek(0)  # rewind to beginning of file
+		import base64
+		Futuredata_png = base64.b64encode(Futurefile.getvalue()).decode()
+		return Futuredata_png
 	
 
 
@@ -117,4 +146,4 @@ if __name__ == '__main__':
 	#plot_temperature('June', 1816, 2012, show_plot=True)
 	#plot_CO2(1751, 2012, show_plot=True)
 	#plot_CO2_country(2012, low_thresh=15, upp_thresh=50, show_plot=True)
-	plot_future(2030, 'June')
+	plot_future(2030, 'June', show_plot=True)
--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -1,36 +1,107 @@
 from flask import Flask
-from flask import render_template
-from temperature_CO2_plotter import plot_temperature, plot_CO2
+from flask import render_template, request
+from temperature_CO2_plotter import plot_temperature, plot_CO2, plot_CO2_country, plot_future
 
 app = Flask(__name__)
 
-@app.route("/")#, methods=['POST'])
+@app.route("/")
 def root():
 	return render_template("root.html")
 
-"""
-def temp():
-	plot_temperature('June', 1816, 2012)
-	plot_CO2(1751, 2012)
-	return render_template("test.html")
-"""
 
-
-@app.route("/plots", methods=['GET', 'POST'])
+@app.route("/plots")
 
 def plots():
 
-	start_yearTemp = int(request.form["startyearTemp"])
-	end_yearTemp = int(request.form["endyearTemp"])
-	plot_temperature('June', start_yearTemp, end_yearTemp)
-
 	return render_template("plots.html")
 
-@app.route("/plots/temp_plot")
 
-def plot():
-	#plot_temperature('June', start_yearTemp, end_yearTemp)
-	return render_template("test.html")
+@app.route("/plots/temp_plot", methods=['GET', 'POST'])
+
+def temp_plot():
+
+	start_yearTemp = int(request.form["startyearTemp"])
+	end_yearTemp = int(request.form["endyearTemp"]) 
+	ymin = request.form["yminTemp"]
+	ymax = request.form["ymaxTemp"]
+	y_axis = [ymin, ymax]
+	if ("Default" in y_axis) or ("" in y_axis):
+		y_axis = None
+	else:
+		y_axis = [float(ymin), float(ymax)]
+
+	month = request.form["month"]
+
+	temp = plot_temperature(month, start_yearTemp, end_yearTemp, y_axis)
+	return render_template("temp_plot.html", temp=temp )
+
+
+@app.route("/plots/CO2_plot", methods=['GET', 'POST'])
+
+def CO2_plot():
+
+	start_yearCO2 = int(request.form["startyearCO2"])
+	end_yearCO2 = int(request.form["endyearCO2"])
+	ymin = request.form["yminCO2"]
+	ymax = request.form["ymaxCO2"]
+	y_axis = [ymin, ymax]
+	if ("Default" in y_axis) or ("" in y_axis):
+		y_axis = None
+	else:
+		y_axis = [float(ymin), float(ymax)]
+
+	co2 = plot_CO2(start_yearCO2, end_yearCO2, y_axis)
+	return render_template("co2_plot.html", co2=co2 )
+
+
+@app.route("/CO2_by_country")
+	
+def country_index():
+
+	return render_template("CO2_by_country.html")
+
+
+@app.route("/CO2_by_country/bar_plot", methods=['GET', 'POST'])
+
+def Bar_plot():
+
+	year_country = int(request.form["yearCountry"])
+
+	low_thresh = float(request.form["lowThresh"])
+	upp_thresh = float(request.form["uppThresh"])
+
+	co2_country = plot_CO2_country(year_country, low_thresh, upp_thresh)
+	return render_template("bar_plot.html", co2_country=co2_country)
+
+
+@app.route("/future")
+
+def future_index():
+
+	return render_template("future.html")
+
+@app.route("/future/future_plot", methods=['GET', 'POST'])
+
+def future_plot():
+
+	fut_month = request.form["futMonth"]
+	fut_year = int(request.form["futYear"])
+
+	future = plot_future(fut_year, fut_month)
+	return render_template("future_plot.html", future=future)
+
+@app.route("/help")
+
+def help():
+
+	return"""<h1><b>Sadly I ran out of time, and wasn't able to complete
+		this help page, as well as finishing writing doc-strings and comments in the codes.
+		I was also unable to fit an error page into my tree of pages properly,
+		but this can by checked out by going to 'http://127.0.0.1:5000/plots/error_temp'.
+		I had big plans for this one, but oh well.
+		</b><h1>
+		"""
+
 
 @app.route("/plots/error_temp")
 
